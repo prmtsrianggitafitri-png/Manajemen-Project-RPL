0ac2bafbb7d8601ad6f119c8a2acbab18e6c07c8
Nadiyah Syahidah Almas <nadiaaiyanadia@gmail.com>
2026-04-24 09:58:31 +0700

    update mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Mahasiswa extends Model
+class Mahasiswa extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $table = 'mahasiswas';
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'email',
+        'jenis_kelamin',
+        'tahun_masuk',
+        'no_telepon',
+        'status',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
 }
+g

b054276ac4c3ecd87810a0a85317615bbe066304
nabilaaauliaputri <nabilaulptr@upi.edu>
2026-05-03 02:16:59 +0700

    update migration admins table

--- database/migrations/2026_04_17_065459_create_admins_table.php
+++ database/migrations/2026_04_17_065459_create_admins_table.php
@@ -1,30 +1,10 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up()
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->id('NPSN');
-            $table->string('nama');
-            $table->string('email');
-            $table->string('password');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('admins');
-    }
-};
+    Schema::create('admins', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('npsn')->unique();
+        $table->string('password');
+        $table->timestamps();
+    });
+}
